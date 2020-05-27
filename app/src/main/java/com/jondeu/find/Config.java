e4a69911112b43a865360664b559b43690d58e3f
interglare <interglare@gmail.com>
2020-05-27 12:51:41 +0600

    Initial commit

---
+++ app/src/main/java/com/jondeu/find/Config.java
@@ -0,0 +1,11 @@
+package com.jondeu.find;
+
+public class Config {
+
+    //админ панель
+    public static final String ADMIN_PANEL_URL = "http://jondeufind.kl.com.ua";
+    public static final boolean ENABLE_DECIMAL_ROUNDING = true;
+    public static final boolean ENABLE_RTL_MODE = false;
+    public static final int SPLASH_TIME = 3000;
+
+}
